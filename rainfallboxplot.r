141d05c64d0a892d2a2f88b76e47a02175c1cf9a
parsazahedi <pz24aab@herts.ac.uk>
2024-11-24 18:35:18 +0000

    Delete rainfallboxplot.r

--- rainfallboxplot.r
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-rainfall <- read_csv("rainfall.csv")
-rfWide<-spread(rainfall,Month,Rainfallmm)
-col_order <- c("Year","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec")
-rfWide2<-rfWide[,col_order]
-boxplot(rfWide2[-c(1)])
