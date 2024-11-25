9c9c97ec98e8be2849972679aa1efc39dc7338e5
vp24abd <vp24abd@herts.ac.uk>
2024-11-25 08:25:45 +0000

    Resolved Bugs

--- R code team research with some issues.R
+++ R code team research with some issues.R
@@ -15,7 +15,7 @@ plot(
 abline(h = mean(selected_data$W_PCT, na.rm = TRUE), col = "blue", lwd = 1)
 
 R version 4.4.2 (2024-10-31 ucrt) -- "Pile of Leaves"
-Copyright (C) 2024 The R Foundation for Statistical Computing
+Copyright (C) 2024 The R Foundation for Statistical Computing#
 Platform: x86_64-w64-mingw32/x64
 
 R is free software and comes with ABSOLUTELY NO WARRANTY.
